8f0854fdb4860396b1b3beed7b1e63bd8f007075
Gustavo Araujo <gustavodzn@live.com>
2024-07-17 19:20:26 -0300

    first commit

---
+++ src/rest-with-springboot-aws/src/test/java/br/com/gustavodvp/restwithspringbootaws/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavodvp.restwithspringbootaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
